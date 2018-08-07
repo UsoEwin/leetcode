9a5aebd7fa063b7c23326ff53f41b89ba6a5b122
usoewin <insomniacui@hotmail.com>
2018-08-06 22:48:12 -0700

    initial

---
+++ leetcode.cpp
@@ -0,0 +1,291 @@
+//anagramMap
+class Solution {
+public:
+    vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
+        int * hash = new int[100000];
+        vector<int> result;
+        for (int i = 0; i < B.size(); ++i)
+        {
+        	hash[B[i]] = i;
+        }
+        for (int j = 0; j < A.size(); ++j)
+        {
+        	result.push_back(hash[A[j]]);
+        }
+        return result;
+    }
+};
+//check possibility
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+		int counter = 0;
+		for (int i = 1; i < nums.size()-1; ++i)
+		      {
+
+		      	if (nums[i-1] > nums[i] && nums[i]>nums[i+1])
+		      	{
+		      		counter+=2;
+		      	}
+		      	else if(nums[i-1]>nums[i]) counter++;
+		      	
+		      	if (counter >= 2)
+		      	{
+		      		return false;
+		      	}
+		      }  
+		return true;    
+    }
+};
+//hamming dis
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        bitset<32> xBS(x);
+        bitset<32> yBS(y);
+        int count = 0;
+        for(int i = 0; i < 32; i++)
+        {
+            if(xBS[i] != yBS[i])
+            {
+                count++;
+            }
+        }
+        return abs(count);
+    }
+};
+
+//
+class Solution {
+public:
+    int findComplement(int num) {
+        bitset<32> numBS(num);
+        int result = 0;
+        bitset<32> = rBS(result)
+        int count = 0;
+        for (int i = 0; i < 32; ++i)
+        {	
+        	if (numBS[i] != 0)
+        	{
+        		break;
+        	}
+        	count++;
+        }
+        for (int i = count; i < count; ++i)
+        {
+        	/* code */
+        }
+    }
+};
+//jewler
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int hash[500000] = {0};
+        int counter =0 ;
+        for(int i = 0; i < J.length(); ++i){
+            hash[J[i]]++;
+        }
+        for( int i = 0; i < S.length(); ++i){
+            if(hash[S[i]]!=0) counter++;
+        }
+        return counter;
+    }
+};
+//
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int hash[1000] = {0};
+        for(int i = 0; i<moves.length();i++){
+            hash[moves[i]]++;
+        }
+        char up = 'U',dn='D',lf='L',ri='R';
+        if((hash[up]!=hash[dn])||(hash[lf]!=hash[ri]))
+            return false;
+        return true;
+    }
+};
+//
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        string buffer;
+        for(int i = 1; i<=n;i++ ){
+            if(i%15==0) buffer="FizzBuzz";
+            else if(i%5==0) buffer="Buzz";
+            else if(i%3==0) buffer="Fizz";
+            else buffer = to_string(i);
+            
+            result.push_back(buffer);
+        }
+        return result;
+    }
+};
+//
+class Solution {
+public:
+    string reverseString(string s) {
+    
+        reverse(s.begin(),s.end());    
+        return s;
+    }
+};
+//
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> vec;
+        for(int i = left; i <= right; i++)
+        {
+            int val = i, num = i;
+            int tmp = 0;
+            while(num)
+            {
+                if(num / 10 >= 1 && num != 10) //num > 10
+                {
+                    tmp = num % 10;
+                    if(tmp == 0)
+                        break;
+                    else
+                    {
+                        if(val % tmp == 0)
+                        {
+                            num /= 10;
+                            continue;
+                        }
+                        else
+                            break;
+                    }
+                }
+                else if(num == 10) //num = 10
+                {
+                    break;
+                }
+                else if(num / 10 == 0) //num < 10
+                {
+                    if(val % num == 0)
+                        vec.push_back(val);
+                    break;
+                }
+            }
+        }
+        return vec;
+    }
+};
+//
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> all;
+        for(int i=0;i<nums.size();i++){
+            all[nums[i]] +=1;
+        }
+        for(map<int,int>::iterator it=all.begin(); it!=all.end(); it++){
+            if(it->second>nums.size()/2) return it->first;
+        }
+        return 0;
+    }
+};
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<char> buffer;
+        string result;
+        for(int i = 0; i < s.length(); i++){
+            buffer.push_back(s[i]); //need to check ' '
+            if(s[i] == ‘ ’){
+                for(j = 0; j < buffer.size();j++){
+                    result = result + buffer.back();
+                    buffer.pop_back();
+                }
+            }
+        }
+        return result;
+    }
+};
+
+class Solution
+{
+public:
+    string reverseWords(string s) {
+        std::vector<char> buffer;
+        string result;
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] != ' ')
+            {
+                buffer.push_back(s[i]);
+            }
+            if (s[i] == ' ')
+            {
+                int k = buffer.size();
+                for (int j = 0; j < k; ++j)
+                {
+                    result = result + buffer.back();
+                    buffer.pop_back();
+                }
+                result+=' ';
+            }
+        }
+            int k = buffer.size();
+                for (int j = 0; j < k; ++j)
+                {
+                    result = result + buffer.back();
+                    buffer.pop_back();
+                }
+        return result;
+    }
+    
+};
+class Solution {
+public:
+    string reverseWords(string s)
+    {
+        int spaceIdx = -1;
+        const int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == ' ' || i == n - 1)
+            {
+                if (i == n - 1)
+                    i++;
+                int l = spaceIdx + 1;
+                int r = i - 1;
+                while (l < r)
+                    swap(s[l++], s[r--]);
+                spaceIdx = i;
+            }
+        }
+        return s;
+    }
+};
+//keyboard
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        vector<string> ret;
+        for(int i = 0; i<words.size();++i){
+            if(std::regex_match(words[i],regex("[qwertyuiopQWERTYUIOP]*")) || 
+              std::regex_match(words[i],regex("[asdfghjklASDFGHJKL]*")) ||
+              std::regex_match(words[i],regex("[zxcvbnmZXCVBNM]*")))
+                ret.push_back(words[i]);
+        
+        }
+        return ret;
+    }
+};
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int max1 = 0,counter = 0;
+        for(int i = 0; i<nums.size();++i){
+            if(nums[i] == 0) max1 = counter>max1?counter:max1,counter = 0;
+            else counter++;
+        }
+        max1 = counter>max1?counter:max1;
+        return max1;
+    }
+};
