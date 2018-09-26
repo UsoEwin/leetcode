419f8dc64a91a5986a924b1dddb85fd1b8538e4a
JING WU <insomniacui@hotmail.com>
2018-09-25 22:40:56 -0700

    one more

--- leetcode.cpp
+++ leetcode.cpp
@@ -1188,3 +1188,27 @@ public:
         return ret;
     }
 };
+//807. Max Increase to Keep City Skyline
+static const auto io_sync_off = [] () {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> col(n, 0), row(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i] = max(row[i], grid[i][j]);
+                col[j] = max(col[j], grid[i][j]);
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                res += min(row[i], col[j]) - grid[i][j];
+        return res;
+    }
+};
