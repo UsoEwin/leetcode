331ce2afba19e28c9aeaff394b0c0895efc0313e
JING WU <insomniacui@hotmail.com>
2018-09-27 20:51:36 -0700

    one more

--- leetcode.cpp
+++ leetcode.cpp
@@ -1254,3 +1254,43 @@ public:
             return searchBST(root->left,val);
     }
 };
+//860. Lemonade Change
+static const auto io_sync_off = [] () {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int dollar5 = 0, dollar10 = 0;
+        for(int i = 0; i < bills.size(); ++i)
+        {
+            if(bills[i] == 5)
+            {
+                dollar5++;
+            }
+            else if(bills[i] == 10)
+            {
+                dollar5--,dollar10++;
+                if(dollar5<0)
+                    return false;
+            }
+            else
+            {
+                if(dollar10 > 0 && dollar5 > 0)
+                {
+                    dollar10--,dollar5--;
+                }
+                else if(dollar10 == 0 && dollar5 > 2)
+                {
+                    dollar5-=3;
+                }
+                else
+                    return false;
+                    
+            }
+        }
+        return true;
+    }
+};
