ff99b7f1a7adbebaea7edd2da7b05eee44e1f957
JING WU <insomniacui@hotmail.com>
2018-09-30 09:29:21 -0700

    one more

--- leetcode.cpp
+++ leetcode.cpp
@@ -1416,3 +1416,12 @@ public:
         return ret;
     }
 };
+//342. Power of Four
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num == 0) return false;
+        else if(num == 1)return true;
+        return (((num&(num-1)) == 0) && ((num-1)%3 == 0));
+    }
+};
