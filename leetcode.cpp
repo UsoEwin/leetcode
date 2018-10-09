8b71a6c3c92dd9814b5f7fa4c38d863320ac4d53
JING WU <insomniacui@hotmail.com>
2018-10-08 19:40:27 -0700

    237. Delete Node in a Linked List

--- leetcode.cpp
+++ leetcode.cpp
@@ -1883,3 +1883,11 @@ public:
         return ret;
     }
 };
+//237. Delete Node in a Linked List
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
