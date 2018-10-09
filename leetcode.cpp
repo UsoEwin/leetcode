de6e579a84375afdea0e117a7ce5f49cb0407765
JING WU <insomniacui@hotmail.com>
2018-10-08 19:37:48 -0700

    /448. Find All Numbers Disappeared in an Array

--- leetcode.cpp
+++ leetcode.cpp
@@ -1850,3 +1850,21 @@ private:
         return;
     }
 };
+//448. Find All Numbers Disappeared in an Array
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ret;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            int correctIdx = abs(nums[i]) - 1;
+            nums[correctIdx] = nums[correctIdx] > 0 ? -nums[correctIdx]:nums[correctIdx];
+        }
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums[i] > 0)
+                ret.push_back(i+1);
+        }
+        return ret;
+    }
+};
