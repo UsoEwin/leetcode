8f4d8b66d3354215770364976ec4f0ad81c9b35a
usoewin <insomniacui@hotmail.com>
2018-10-13 16:21:48 -0700

    724. Find Pivot Index

--- leetcode.cpp
+++ leetcode.cpp
@@ -2070,3 +2070,22 @@ public:
         
     }
 };
+//724. Find Pivot Index
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            sum += nums[i];
+        }
+        int curr = 0;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if((curr * 2) == sum - nums[i])
+                return i;
+            curr += nums[i];
+        }
+        return -1;
+    }
+};
