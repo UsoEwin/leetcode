1dd6fd2c0200967ca34cf336f3047b2a5b74e6b7
Jingbo Wu <insomniacui@hotmail.com>
2018-10-16 18:00:19 -0700

    293. Flip Game

--- leetcode.cpp
+++ leetcode.cpp
@@ -2250,3 +2250,25 @@ public:
         return res;
     }
 };
+//293. Flip Game
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string s) {
+        vector<string> res;
+        if(s.length() < 2) return res;
+        for(int i = 0; i < s.length()-1; ++i)
+        {
+            if(s[i] == s[i+1])
+                {
+                    if(s[i] == '+')
+                        {
+                            string temp = s;
+                            temp[i] = temp[i+1] = '-';
+                            res.push_back(temp);
+                        }
+
+                }
+        }
+        return res;
+    }
+};
