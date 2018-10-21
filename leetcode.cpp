33b95d04651dd1411f164ce8dcd66af6eb8fc842
Jingbo Wu <insomniacui@hotmail.com>
2018-10-21 14:03:15 -0700

    191. Number of 1 Bits

--- leetcode.cpp
+++ leetcode.cpp
@@ -2448,3 +2448,16 @@ public:
         return res;
     }
 };
+//191. Number of 1 Bits
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int sum = 0;
+        while(n>0)
+        {
+            sum++;
+            n &= (n-1);
+        }
+        return sum;
+    }
+};
