1058907c1a708423d2615d91c9bdeec37b29f3b5
Jingbo Wu <insomniacui@hotmail.com>
2018-10-26 09:21:14 -0700

    556. Reshape the Matrix

--- leetcode.cpp
+++ leetcode.cpp
@@ -2600,3 +2600,22 @@ public:
         return res;
     }
 };
+//556. Reshape the Matrix
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        vector<vector<int>> res(r,vector<int>(c));
+        int count = 0;
+        if(r*c != nums.size()*nums[0].size())
+            return nums;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            for(int j = 0; j < nums[i].size(); ++j)
+            {
+                res[count/c][count%c] = nums[i][j];
+                count++;
+            }
+        }
+        return res;
+    }
+};
