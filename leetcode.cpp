6d5fb22e01bcc2482dc774760b304dafad2fc561
Jingbo Wu <insomniacui@hotmail.com>
2018-11-05 10:45:15 -0800

    521. Longest Uncommon Subsequence

--- leetcode.cpp
+++ leetcode.cpp
@@ -3006,3 +3006,14 @@ public:
         return res;
     }
 };
+//521. Longest Uncommon Subsequence
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if(a == b) return -1;
+        else if(a.size() == b.size())
+            return a.size();
+        else
+            return max(a.size(),b.size());
+    }
+};
