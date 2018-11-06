f467a28606519d5762e5ff799e000ebd551be9f3
Jingbo Wu <insomniacui@hotmail.com>
2018-11-05 20:36:25 -0800

    118. Pascal's Triangle

--- leetcode.cpp
+++ leetcode.cpp
@@ -3017,3 +3017,35 @@ public:
             return max(a.size(),b.size());
     }
 };
+//118. Pascal's Triangle
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if(numRows <= 0)
+            return res;
+        res.push_back({1});
+        if(numRows == 1)
+            return res;
+        res.push_back({1,1});
+        if(numRows == 2)
+            return res;
+        for(int i = 2; i < numRows; ++i)
+        {
+            vector<int> temp;
+            for(int j = 0; j <= i; ++j)
+            {
+                if(j == 0)
+                    temp.push_back(1);
+                else if(j == i)
+                    temp.push_back(1);
+                else
+                    temp.push_back(res[i-1][j] + res[i-1][j-1]);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    
+            
+    }
+};
