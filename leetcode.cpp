aca5af5890f4d42f68e2f92c93d97b1b2a0e1782
Jingbo Wu <insomniacui@hotmail.com>
2018-11-11 10:30:04 -0800

    645. Set Mismatching

--- leetcode.cpp
+++ leetcode.cpp
@@ -3175,3 +3175,27 @@ public:
         return (target - sum) % 2 == 0 ? step - 1 : step + (step-1) %2;
     }
 };
+//645. Set Mismatching
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> res;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums[abs(nums[i])-1] < 0)
+                res.push_back(abs(nums[i]));
+            else
+                sum += nums[i], nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
+        }
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums[i] > 0)
+            {
+                res.push_back(i+1);
+                break;
+            }
+        }
+        return res;
+    }
+};
