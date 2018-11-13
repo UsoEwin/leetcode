b078aab98d9755687f24f564d93719b2de59844b
Jingbo Wu <insomniacui@hotmail.com>
2018-11-12 22:24:22 -0800

    414. Third Maximum Number

--- leetcode.cpp
+++ leetcode.cpp
@@ -3289,3 +3289,31 @@ public:
         return cnt;
     }
 };
+//414. Third Maximum Number
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long max1 = LLONG_MIN, max2 = LLONG_MIN, max3 = LLONG_MIN;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(nums[i] == max1 || nums[i] == max2 || nums[i] == max3)
+                continue;
+            if(max1 == LLONG_MIN || nums[i] > max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(max2 == LLONG_MIN || nums[i] > max2)
+            {
+                max3 = max2;
+                max2 = nums[i];
+            }
+            else if(max3 == LLONG_MIN || nums[i] > max3)
+            {
+                max3 = nums[i];
+            }
+        }
+        return max3 == LLONG_MIN ? max1 : max3;
+    }
+};
