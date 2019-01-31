fc5175fdce75291f97a39fee5bca0a4ba8ba1ec0
UsoEwin <insomniacui@hotmail.com>
2019-01-30 23:33:44 -0800

    219. Contains Duplicate II

--- leetcode.cpp
+++ leetcode.cpp
@@ -3333,3 +3333,23 @@ public:
         return fib2;
     }
 };
+//219. Contains Duplicate II
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mymap;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(mymap.find(nums[i]) == mymap.end())
+                mymap[nums[i]] = i;
+            else
+            {
+                if(i-mymap[nums[i]] <= k)
+                    return true;
+                else
+                    mymap[nums[i]] = i;
+            }
+        }
+        return false;
+    }
+};
