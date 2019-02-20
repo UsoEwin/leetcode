f6b40f30344f4efde7dcb1257654a28bf69be524
UsoEwin <insomniacui@hotmail.com>
2019-02-20 10:03:03 -0800

    50. Pow(x, n)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3353,3 +3353,25 @@ public:
         return false;
     }
 };
+//50. Pow(x, n)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N < 0)
+        {   
+            N = -N; 
+            x = 1/x;
+        }
+        return fastPow(x,N);
+    }
+private:
+    double fastPow(double x, int n) {
+        if(n == 0) return 1.0;
+        double half = fastPow(x, n/2);
+        if(n % 2 == 0)
+            return half * half;
+        else 
+            return half * half * x;
+    }
+};
