f17662eed9317f7976900e89d0feb4447066b33a
Sulumia <insomniacui@hotmail.com>
2019-07-28 21:32:46 -0700

    80. Remove Duplicates from Sorted Array II

--- leetcode.cpp
+++ leetcode.cpp
@@ -4596,3 +4596,16 @@ public:
         return f2;
     }
 };
+//80. Remove Duplicates from Sorted Array II
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int idx = 2;
+        if(nums.size() <= 2) return nums.size();
+        for(int i = 2; i < nums.size(); ++i) {
+            if(nums[i] != nums[idx-2])
+                nums[idx++] = nums[i];
+        }
+        return idx;
+    }
+};
