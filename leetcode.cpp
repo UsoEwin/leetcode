c2ac09c1960020b7b8477638ab461f0bb7a28439
Sulumia <insomniacui@hotmail.com>
2019-07-28 17:09:29 -0700

    836. Rectangle Overlap

--- leetcode.cpp
+++ leetcode.cpp
@@ -4554,3 +4554,11 @@ private:
         return;
     }
 };
+//836. Rectangle Overlap
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        return (min(rec2[2],rec1[2]) > max(rec1[0],rec2[0]))
+                && (min(rec1[3],rec2[3]) > max(rec1[1],rec2[1]));
+    }
+};
