1db61157e8667e86e462dd79675761e44188ea12
Sulumia <insomniacui@hotmail.com>
2019-07-29 22:09:17 -0700

    628. Maximum Product of Three Numbers

--- leetcode.cpp
+++ leetcode.cpp
@@ -4662,3 +4662,33 @@ public:
         return false;
     }
 };
+//628. Maximum Product of Three Numbers
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int max1 = -1001, max2 = -1001, max3=-1001;
+        int min1 = 1001, min2 = 1001;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(nums[i] >= max2) {
+                max3 = max2;
+                max2 = nums[i];
+            }
+            else if(nums[i] >= max3) {
+                max3 = nums[i];
+            }
+            if(nums[i] <= min1) {
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if(nums[i] <= min2) {
+                min2 = nums[i];
+            }
+        }
+        return max(max1*max2*max3,max1*min1*min2);
+    }
+};
