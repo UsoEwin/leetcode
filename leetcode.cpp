f18c88c0e5b73e9906ebbddc5222ee74dca82c8b
Sulumia <insomniacui@hotmail.com>
2019-07-31 21:14:58 -0700

    172. Factorial Trailing Zeroes

--- leetcode.cpp
+++ leetcode.cpp
@@ -4828,3 +4828,10 @@ public:
         return false;
     }
 };
+//172. Factorial Trailing Zeroes
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        return n < 5 ? 0 : n/5 + trailingZeroes(n/5);
+    }
+};
