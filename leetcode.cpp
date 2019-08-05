750631cdebd4810d026f6f13ee99bd90d1094ae6
Sulumia <insomniacui@hotmail.com>
2019-08-04 20:21:29 -0700

    37. Sudoku Solver

--- leetcode.cpp
+++ leetcode.cpp
@@ -4991,3 +4991,45 @@ public:
 
     }
 };
+//37. Sudoku Solver
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        if(board.empty() || board.size() != 9 || board[0].size() != 9)
+            return;
+        dfs(board,0,0);
+    }
+private:
+    bool dfs(vector<vector<char>>& board, int i, int j) {
+        if(i >= 9) return true;
+        if(j >= 9) return dfs(board,i+1,0);
+        if(board[i][j] == '.') {
+            for(char k = '1'; k <= '9'; ++k) {
+                board[i][j] = k;
+                if(isValid(board,i,j) && dfs(board,i,j+1))
+                    return true;
+                board[i][j] = '.';
+            }
+        }
+        else 
+            return dfs(board,i,j+1);
+        return false;
+    }
+    bool isValid(vector<vector<char>>& board, int i, int j) {
+        for(int col = 0; col < 9; ++col) {
+            if(j != col && board[i][j] == board[i][col])
+                return false;
+        }
+        for(int row = 0; row < 9; ++row) {
+            if(i != row && board[i][j] == board[row][j])
+                return false;
+        }
+        for(int row = i/3*3; row < i/3*3 + 3; ++row) {
+            for(int col = j/3*3; col < j/3*3 + 3; ++col) {
+                if(row != i && col != j && board[row][col] == board[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
