eef4c9e8c0ecabd14735997d69e4cf50053422c7
Sulumia <insomniacui@hotmail.com>
2019-08-05 23:25:39 -0700

    73. Set Matrix Zeroes

--- leetcode.cpp
+++ leetcode.cpp
@@ -5071,3 +5071,32 @@ public:
         return res;
     }
 };
+//73. Set Matrix Zeroes
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool is_col_0 = false;
+        int r = matrix.size(), c = matrix[0].size();
+        for(int i = 0; i < r; ++i) {
+            if(matrix[i][0] == 0)
+                is_col_0 = true;
+            for(int j = 1; j < c; ++j) {
+                if(matrix[i][j] == 0)
+                    matrix[i][0] = 0, matrix[0][j] = 0;
+            }
+        }
+            for(int i = 1; i < r; ++i) {
+                for(int j = 1; j < c; ++j) 
+                    if(matrix[i][0] == 0 ||matrix[0][j] == 0)
+                        matrix[i][j] = 0;
+            }
+            if(matrix[0][0] == 0) {
+                for(int j = 0; j < c; ++j)
+                    matrix[0][j] = 0;
+            }
+            if(is_col_0) {
+                for(int i = 0; i < r; ++i)
+                    matrix[i][0] = 0;
+            }
+    }
+};
